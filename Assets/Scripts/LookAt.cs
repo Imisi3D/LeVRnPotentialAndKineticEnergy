88c915278a19fefca07cfefacd6704699755f733
Oussama Belhadj Hmida <belhadj_hmida@outlook.fr>
2020-01-26 17:07:39 +0100

    Imported assets

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public GameObject target;
+
+    void Update()
+    {
+        if(target != null)
+        {
+            transform.rotation = Quaternion.LookRotation(transform.position - target.transform.position);
+        }        
+    }
+}
